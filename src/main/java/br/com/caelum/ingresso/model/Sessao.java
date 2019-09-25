2a9ed866e904f72f03e7cf9901d68bd661aa81e4
Karina Domingues <kdomingues1@gmail.com>
2019-09-25 20:25:54 -0300

    Adicionando a sessão controller

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Sessao {
@@ -13,10 +14,59 @@ public class Sessao {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	Filme filme;
+	@ManyToOne //COMPORTAMENTO EAGER
+	private Filme filme;
 	
-	Sala sala;
+	@ManyToOne
+	private Sala sala;
 	
-	LocalTime horario;
+	private LocalTime horario;
+	
+	
+	/**
+	 * @deprecated hibernate only
+	 */
+
+	public Sessao() {
+		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
+	public Sessao(Filme filme, Sala sala, LocalTime horario) {
+		this.filme = filme;
+		this.sala = sala;
+		this.horario = horario;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
 
 }
